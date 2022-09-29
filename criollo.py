349eb9a7a4e62fa5ec5656bfea459c299c68b638
pellto <lovegkgns@gmail.com>
2022-09-29 17:46:11 +0900

    improvement inference speed

--- criollo.py
+++ criollo.py
@@ -4,6 +4,9 @@ import re
 from typing import Union, Dict
 
 from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from utils import CriolloDataset
 
 
 class Criollo:
@@ -36,7 +39,8 @@ class Criollo:
 
     def __parse(self):
         self.room_name = self.data[:self.data.find('님과 카카오톡 대화')].split(" ")[0]
-        parser = re.compile(r'\[(\S+)] \[(\S{2} \d{1,2}:\d{1,2})] ([\s\S]+?)\n', re.MULTILINE)
+        parser = re.compile(
+            r'\[(\S+)] \[(\S{2} \d{1,2}:\d{1,2})] ([\s\S]+?)\n', re.MULTILINE)
         arr = []
         for line in parser.finditer(self.data):
             user, time, text = line.groups()
@@ -103,7 +107,8 @@ class Criollo:
             if text not in text_counts:
                 text_counts[text] = 9
             text_counts[text] += 1
-        text_counts = sorted(text_counts.items(), key=lambda x: x[1], reverse=True)[:k]
+        text_counts = sorted(text_counts.items(),
+                             key=lambda x: x[1], reverse=True)[:k]
         ret = dict()
         for key, value in text_counts:
             ret[key] = value
@@ -121,7 +126,8 @@ class Criollo:
                 ret[user][text] = 0
             ret[user][text] += 1
         for user in ret:
-            temp = sorted(ret[user].items(), key=lambda x: x[1], reverse=True)[:top_k]
+            temp = sorted(ret[user].items(),
+                          key=lambda x: x[1], reverse=True)[:top_k]
             ret[user] = dict()
             for k, v in temp:
                 ret[user][k] = v
@@ -139,47 +145,51 @@ class Criollo:
                 ret[user][text] = 0
             ret[user][text] += 1
         for user in ret:
-            temp = sorted(ret[user].items(), key=lambda x: x[1], reverse=True)[:top_k]
+            temp = sorted(ret[user].items(),
+                          key=lambda x: x[1], reverse=True)[:top_k]
             ret[user] = dict()
             for k, v in temp:
                 ret[user][k] = v
         return ret
-    
+
     def __initialize_ai(self):
         import torch
-        from torch.utils.data import DataLoader
-        from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+        from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
-        from utils import CriolloDataset
-
-        self.tok = AutoTokenizer.from_pretrained("jaehyeong/koelectra-base-v3-generalized-sentiment-analysis")
-        self.model = AutoModelForSequenceClassification.from_pretrained("jaehyeong/koelectra-base-v3-generalized-sentiment-analysis")
+        self.tok = AutoTokenizer.from_pretrained(
+            "jaehyeong/koelectra-base-v3-generalized-sentiment-analysis")
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            "jaehyeong/koelectra-base-v3-generalized-sentiment-analysis")
         device = 0 if torch.cuda.is_available() else -1
-        self.clf = TextClassificationPipeline(tokenizer=self.tok, model=self.model, device=device)
-
-        dataset = CriolloDataset(self.arr)
-        self.dataloader = DataLoader(dataset=dataset, batch_size=128, shuffle=False)
+        self.clf_pipe = pipeline(task='text-classification', tokenizer=self.tok,
+                                 model=self.model, device=device, use_fast=True, framework='pt')
         self.ai_initialized = True
-    
+
     def sent_cls(self):
         assert self.parsed
         if not self.ai_initialized:
             self.__initialize_ai()
+
+        dataset = CriolloDataset(self.arr)
+        dataloader = DataLoader(
+            dataset=dataset, num_workers=4, shuffle=False, batch_size=64)
+
         result = dict()
-        with tqdm(self.dataloader) as pbar:
+        with tqdm(dataloader) as pbar:
             pbar.set_description("Analyzing text")
             for batch in pbar:
-                users, times, texts = batch[0], batch[1], list(batch[2])
-                preds = self.clf(texts)
-                for idx in range(len(preds)):
-                    pred, user, time = preds[idx], users[idx], times[idx]
+                users, times, texts = batch
+                texts = list(texts)
+                preds = self.clf_pipe(texts)
+                for i in range(len(preds)):
+                    pred, user = preds[i], users[i]
                     if user not in result:
                         result[user] = []
-                    
+
                     label, score = pred['label'], pred['score']
                     if score < 0.75:
                         continue
-                    
+
                     if label == 0:
                         result[user].append(-score)
                     else:
